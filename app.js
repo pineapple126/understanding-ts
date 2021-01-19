696bc2ee1cf2ad0ebd74554277f9ac7064002393
pineapple126 <824090342@qq.com>
2021-01-19 19:49:20 +0800

    Function Types and Callbacks

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ function add(n1, n2) {
 function printResult(num) {
     console.log("Result: " + num);
 }
+function addAndHandle(n1, n2, cb) {
+    var result = n1 + n2;
+    cb(result);
+}
 printResult(add(5, 12));
 var combineValues;
 combineValues = add;
@@ -11,3 +15,6 @@ combineValues = add;
 // combineValues = 5;
 console.log(combineValues(8, 8));
 // let someValue: undefined;
+addAndHandle(10, 20, function (result) {
+    console.log(result);
+});
