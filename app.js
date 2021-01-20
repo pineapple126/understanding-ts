7be8f397921307d3c094da2009cf915c58345d1d
pineapple126 <824090342@qq.com>
2021-01-20 23:45:44 +0800

    Compiler the Entire Project

--- app.js
+++ app.js
@@ -1,12 +1,2 @@
-var userInput;
-var userName;
-userInput = 5;
-userInput = "Max";
-if (typeof userInput === "string") {
-    userName = userInput;
-}
-function generateError(message, code) {
-    throw { message: message, errorCode: code };
-    // while (true) {}
-}
-generateError("An error occurred!", 500);
+"use strict";
+console.log("zhaoq");
--- app.ts
+++ app.ts
@@ -1,15 +1,1 @@
-let userInput: unknown;
-let userName: string;
-
-userInput = 5;
-userInput = "Max";
-if (typeof userInput === "string") {
-  userName = userInput;
-}
-
-function generateError(message: string, code: number): never {
-  throw { message: message, errorCode: code };
-  // while (true) {}
-}
-
-generateError("An error occurred!", 500);
+console.log("zhaoq");
